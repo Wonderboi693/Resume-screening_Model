422577d763930cc817f47b4199f418bd5bf192b4
Wonderboi693 <nlmhuy0101@gmail.com>
2026-04-29 16:58:48 +0700

    added EDA and a dataset

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ def get_requirements(file_path:str) -> List[str]:
     '''
 
     requirements = []
-    with open(file_path, "rb") as f:
+    with open(file_path) as f:
         requirements = [line.strip() for line in f.readlines()]
     return requirements
 
 
 setup(
-    name = 'Job_Recommendation_System',
+    name = 'Resume-screening_Model',
     version = '0.0.1',
     author = 'Wonderboi693',
     author_email = 'nlmhuy0101@gmail.com',
